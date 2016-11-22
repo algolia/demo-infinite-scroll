44183e55315f7b1f2a134ced6165cb476e99ab56
Pixelastic <tim@pixelastic.com>
2016-11-22 13:10:38 +0100

    Copying content from github.com/algolia/examples/infinite-scroll

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: '#source-map',
+  entry: {
+    app: ['./src/js/app.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/app.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'body'
+    }),
+    new ExtractTextPlugin('css/[name].css')
+  ],
+  module: {
+    loaders:[
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loader: 'url-loader?limit=8192'
+      },
+      {
+        test:  /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      }
+    ]
+  },
+  watch: true
+}
